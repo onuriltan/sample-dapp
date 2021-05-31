a6bf4a3bc04c93dc949772432403a35b7ac8b9f4
onuriltan <onur.iltan@gmail.com>
2021-05-31 10:32:06 +0300

    feat: add sample form to change the value in local eth network

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 1. `yarn add ethers hardhat @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers`
 2. `npx hardhat` to initialize the smart contract
+   ![Alt text](https://github.com/onuriltan/sample-dapp/blob/master/public/Screenshot%202021-05-30%20at%2012.48.03.png?raw=true "Optional title")
 3. `npx hardhat compile` to compile the Greeter.sol and create the smart contract
 4. We will use abi(application binary interface) inside of `src/artifacts/contracts/Greeter.sol/Greeter.json`
    We are going to be importing this to our frontend app to interact with the smart contract
--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import GreeterContract from "./artifacts/contracts/Greeter.sol/Greeter.json";
 
-import { useState } from 'react'
-import { ethers } from 'ethers'
-import GreeterContract  from './artifacts/contracts/Greeter.sol/Greeter.json'
+// This will be different in your local env
+const greeterContractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 const App = () => {
+  const [greeting, setGreetingValue] = useState('');
+
+  // will connect to metamask wallet
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  };
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        GreeterContract.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet(); // value from blockchain
+        console.log("Data: " + data);
+      } catch (e) {
+        console.log("Error: ", e);
+      }
+    }
+  };
+
+  const setGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        greeterContractAddress,
+        GreeterContract.abi,
+        signer
+      );
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue("");
+      await transaction.wait();
+      fetchGreeting();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchGreeting}>Fetch Greeting</button>
+      <button onClick={setGreeting}>Set Greeting </button>
+      <input
+        onChange={(e) => setGreetingValue(e.target.value)}
+        value={greeting}
+        placeholder="Set Greeting"
+      />
     </div>
   );
 };
-
 
 export default App;
